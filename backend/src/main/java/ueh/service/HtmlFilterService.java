7745d510fa324cbb865ec8ba05e38b042c11ac83
Dinh Tan Loc <dinhtanloc.dynamicueh@gmail.com>
2024-12-03 07:46:24 +0700

    update frontend backend

--- backend/src/main/java/ueh/service/HtmlFilterService.java
+++ backend/src/main/java/ueh/service/HtmlFilterService.java
@@ -3,13 +3,41 @@ package ueh.service;
 import ueh.model.HtmlData;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.safety.Safelist;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HtmlFilterService {
-    public String filterHtml(String rawHtml) {
+
+    @Autowired
+    private QueueService queueService;
+
+    /**
+     * Phân loại các thẻ HTML chứa nội dung và đẩy vào queue.
+     */
+    public void classifyAndEnqueueContent(String rawHtml) {
         Document document = Jsoup.parse(rawHtml);
-        return Jsoup.clean(document.body().html(), Safelist.simpleText());
+        Elements contentElements = document.select("p, h1, h2, h3, h4, h5, h6, div, span");
+
+        for (Element element : contentElements) {
+            HtmlData htmlData = new HtmlData(element.outerHtml());
+            queueService.enqueue(htmlData);
+        }
+    }
+
+    /**
+     * Xử lý các thẻ HTML trong queue.
+     */
+    public String processQueue() {
+        StringBuilder processedContent = new StringBuilder();
+
+        while (!queueService.isEmpty()) {
+            HtmlData dequeuedHtmlData = queueService.dequeue();
+            processedContent.append(dequeuedHtmlData.getRawHtml());
+        }
+
+        return processedContent.toString();
     }
 }
